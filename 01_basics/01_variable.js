0fa64adde0a7b34023076183096885654239f401
Sankar110000 <136836807+Sankar110000@users.noreply.github.com>
2024-01-29 15:45:01 +0000

    two files added

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 1939908
+let accountEmail = "sankar@gmail.com"
+var accountPassword = "sankar"
+accountCity = "kanpur"
+let accountState
+
+// accountId  = 2 //not allowed
+
+accountEmail = "sankar@yahoo.com"
+accountPassword = "12345"
+accountCity = "Hydrabad"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+
+
+// alert(3 + 3) // we are using node js noe browser
+
+let name = "sankar"
+let age = 14
+let isLoggedin = true
+
+
+// number 
+// bigint
+// string
+// boolean => true/false
+// nulll =>  standalone value
+// undefined 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined);
