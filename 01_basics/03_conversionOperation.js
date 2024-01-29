15f7e123e73790cc99e9f1feab3d57bf6cc7e243
Sankar110000 <136836807+Sankar110000@users.noreply.github.com>
2024-01-29 16:15:26 +0000

    new change added

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "true"
+
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof(valueInNumber));
+
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedin = "";
+
+let booleanIsLoggedin = Boolean(isLoggedin);
+
+// console.log(booleanIsLoggedin);
+
+// 1 => true ; 0 => false
+// "" => false ; "kuch bhi" => true
+
+let someNumber = 123
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+
+
+console.log(typeof stringNumber);
+
