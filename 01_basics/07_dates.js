0c3a34ffb517454527053ef8ce57d01672d50208
Sankar110000 <136836807+Sankar110000@users.noreply.github.com>
2024-01-31 08:47:59 +0000

    dates in js added

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,25 @@
+const date = new Date();
+// console.log(date);
+
+
+// console.log(date.toString());
+// console.log(date.toLocaleTimeString());
+// console.log(date.toDateString());
+// console.log(date.toLocaleDateString());
+
+
+const myDate = new Date(2023,0,3);
+// console.log(myDate.toDateString());
+
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+// console.log(newDate.getDay())
+// console.log(newDate.getTime())
+// console.log(newDate.getMonth())
+// console.log(newDate.getDate())
