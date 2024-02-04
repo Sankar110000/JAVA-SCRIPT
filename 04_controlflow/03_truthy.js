d89eb89ca19a831589cd684cbf7a54d22ca8bf0c
Sankar110000 <136836807+Sankar110000@users.noreply.github.com>
2024-02-04 12:51:56 +0000

    Updated on 04/02/24

---
+++ 04_controlflow/03_truthy.js
@@ -0,0 +1,58 @@
+const userEmail = "s@gmail.com"
+
+// if(userEmail) {
+//     console.log("got userEmail");
+// }else {
+//     console.log("Don't have userEmail");
+// }
+
+
+
+
+
+// flasy values
+
+// false, 0, -0, BigInt 0n, "",null, undefined, NaN
+
+
+
+
+
+
+// truthy values
+
+// "0", "false", " ", [], {}, funtion(){}
+
+
+
+
+
+// if([1, 2, 3, 4].length === 0){
+//     console.log("Array is empty");
+// }
+
+// const emptyObj = {}
+
+// if(Object.keys(emptyObj).length ===  0){
+//     console.log("Object is empty");
+// }
+
+
+
+
+
+
+// Nullish.C operator (??): nul undefined
+
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 15
+// val1 = undefined ?? 15
+
+console.log(val1);
+
+
+const iceTea = 100;
+
+iceTea >= 80 ? console.log("heavy price") : console.log("okey");
